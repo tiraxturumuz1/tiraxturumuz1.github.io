a5aca7d3e5cd37d8fda5827572e7ceedb1ac9d26
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-03 14:26:35 +0330

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="logo">TiraxturApp</a>
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="#features">Features</a></li>
+        <li><a href="/docs">Docs</a></li>
+        <li><a href="/login" className="btn-get-started">Get Started</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
