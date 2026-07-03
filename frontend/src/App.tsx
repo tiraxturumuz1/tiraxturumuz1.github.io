d141c40b3b5d927fc7040da05c8d6f956febc88d
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-03 14:30:35 +0330

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import Router from './Router';
-import './index.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
-  return <Router />;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      {/* اینجا می‌توانید بخش‌های دیگر سایت را اضافه کنید */}
+    </div>
+  );
 }
 
 export default App;
