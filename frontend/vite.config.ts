a5f60c7d4b367bb2171dc657e955faa25e081d97
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-03 12:19:12 +0330

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/daotest.github.io/', // <--- حتماً نام دقیق مخزن خود را اینجا بنویسید
+  base: '/tiraxturumuz1.github.io/', // <--- حتماً نام دقیق مخزن خود را اینجا بنویسید
   build: {
     outDir: 'dist',
   }
