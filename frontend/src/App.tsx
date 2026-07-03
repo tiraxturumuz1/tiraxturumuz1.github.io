5ba19fde2ca639924aa79034cd657762617aebc9
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-03 14:56:30 +0330

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import './index.css'; // حتماً این خط را اضافه کن تا استایل‌های کلی اعمال شوند
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Features from './components/Features'; // اضافه شد
+import Features from './components/Features';
+import Poll from './components/Poll';
+import Footer from './components/Footer'; // اضافه شد
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Features /> {/* اضافه شد */}
+      <Features />
+      <Poll />
+      <Footer /> {/* اضافه شد */}
     </div>
   );
 }
