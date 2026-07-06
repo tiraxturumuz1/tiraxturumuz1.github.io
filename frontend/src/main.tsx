3b5e9a090f1f6bc28c946daad76be26eb76aac71
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-06 11:26:09 +0330

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+// frontend/src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // مطمئن شوید این فایل وجود دارد یا این خط را حذف کنید
+import './index.css'
+import { AuthProvider } from './context/AuthContext' // اضافه شد
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider> {/* بسیار مهم: کل اپلیکیشن باید در AuthProvider باشد */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
