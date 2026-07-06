38317f2d24ee5479ec9825ef2f0fa9d767205b65
Mohammad nourmohammadi <36516242+tiraxturumuz1@users.noreply.github.com>
2026-07-06 15:20:25 +0330

    Create SignIn.tsx

---
+++ frontend/src/components/SignIn.tsx
@@ -0,0 +1,13 @@
+    // frontend/src/components/SignIn.tsx
+    import React from 'react';
+
+    const SignIn = () => {
+      return (
+        <div>
+          <h2>Sign In Page</h2>
+          {/* اینجا فرم ورود را قرار دهید */}
+        </div>
+      );
+    };
+
+    export default SignIn;
